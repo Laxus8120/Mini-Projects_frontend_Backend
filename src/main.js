1595b0016d718e713975583ad6ff2a30595fef51
Hemant Rawat <hemantrawat812@gmail.com>
2024-01-24 07:20:09 +0530

    completed

--- src/main.js
+++ src/main.js
@@ -3,3 +3,25 @@ import '../styles/style.css'
 import '../styles/aside.css'
 import '../styles/main.css'
 import '../styles/utils.css'
+
+
+// Assuming the 'burger' button has an ID of 'burger'
+var burgerButton = document.getElementById('burger');
+
+// Assuming the 'header' has an ID of 'header' and the cross has a class of 'cross'
+var header = document.getElementById('header');
+var crossButton = document.getElementById('cross');
+
+burgerButton.addEventListener('click', function() {
+  header.classList.toggle('show-header');
+  crossButton.style.display = 'block';
+  burgerButton.style.display = 'none';
+});
+
+crossButton.addEventListener('click', function() {
+  header.classList.remove('show-header');
+  crossButton.style.display = 'none';
+  burgerButton.style.display = 'block';
+});
+
+  
