3798cf6d01659b1122f1722699489cb9426e0d02
Hemant Rawat <hemantrawat812@gmail.com>
2024-04-03 20:44:35 +0530

    deleting some repo

--- small_repo1/src/config/serverConfig.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-   export const PORT = process.env.PORT;
-   export const  authToken = process.env.authToken;
-   export const accountSid =  process.env.accountSid;
